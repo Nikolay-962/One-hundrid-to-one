fe6592a6a7885c1ab177d286b716c762c254717b
NIk <64087311+Nikolay-962@users.noreply.github.com>
2022-11-28 22:53:52 +0300

    Чуть исправил

--- One-hundrid-to-one/js/main.js
+++ One-hundrid-to-one/js/main.js
@@ -1,27 +1,36 @@
 "use strict";
-
 let card = document.querySelectorAll('.card');
+
 let out = document.querySelector('.middle');
 let start = document.querySelector('.start');
 let end = document.querySelector('.end');
-let sum = 0;
-let balls = 0;
 
-for (let i = 0; i < card.length; i++) {
-  let num = card[i];
-  num.addEventListener('click', () => {
-    num.classList.toggle('is-flipped');
-    let ball = +card[i].querySelector('.number').innerHTML;
-    sum += ball;
-    out.innerHTML = sum;
-  })
-};
-/*
-function clearStorge() {
-  localStorage.clear();
+let totalOut = out.textContent;
+
+const sum = (a, b) => {
+  return Number(a) + Number(b);
 }
-*/
-// считает баллы.
+
+card.forEach((element) => {
+  const value = element.querySelector('.number').innerHTML;
+  element.addEventListener('click', () => {
+    element.classList.toggle('is-flipped');
+    out.textContent = sum(totalOut, value);
+    totalOut = sum(totalOut, value);
+  });
+})
+start.addEventListener('click', () => {
+  start.textContent = totalSum(allOne, totalOut);
+  out.textContent = '';
+  localStorage.setItem('ball-1', start.textContent);
+})
+
+end.addEventListener('click', () => {
+  end.textContent = totalSum(allTwo, totalOut);
+  out.textContent = '';
+  localStorage.setItem('ball-2', end.textContent);
+})
+
 let error = document.querySelectorAll('.error-btn');
 for (let i = 0; i < error.length; i++) {
   let er = error[i];
@@ -34,42 +43,16 @@ let allOne = localStorage.getItem('ball-1');
 let allTwo = localStorage.getItem('ball-2');
 allOne = Number(allOne);
 allTwo = Number(allTwo);
-//console.log(allOne)
 
-function clearStorge() {
-  localStorage.removeItem();
-}
-// переходит на следующую страницу и показывает local storige
 let question = document.querySelector('.question');
 question.addEventListener('click', () => {
   question.classList.toggle('is-flipped');
-  start.textContent += allOne;
-  end.textContent += allTwo;
-
+  start.textContent = totalSum(allOne, totalOut);
+  end.textContent = totalSum(allTwo, totalOut);
 })
 
-let startOne = start.textContent;
-let endOne = end.textContent;
-startOne = Number(startOne);
-endOne = Number(endOne)
-//console.log(typeof startOne)
-// при клике сумма переходит в левое поле.
-start.addEventListener('click', () => {
-  start.textContent += sum;
-  out.textContent = '';
-  check();
-  localStorage.setItem('ball-1', start.textContent);
-})
-//при клике в правое поле
-end.addEventListener('click', () => {
-  end.textContent += sum;
-  out.textContent = '';
-  check();
-  localStorage.setItem('ball-2', end.textContent);
-})
-
-function check() {
-  sum = '';
+const totalSum = (a, b) => {
+  return Number(a) + Number(b);
 }
 
 
